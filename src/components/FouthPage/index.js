eacbb5481c20c0180697b02f2f67693bd98fb1d3
Ravi5151 <vasamsettiravi5@gmail.com>
2024-08-01 17:16:11 +0530

    Fourth page completed

--- src/components/FouthPage/index.js
+++ src/components/FouthPage/index.js
@@ -8,6 +8,13 @@ import {
   Para,
   Arrow1,
   Arrow2,
+  ArrowContainer,
+  Anchor,
+  Paragraph,
+  StyledContainer,
+  StyledImage,
+  Line,
+  StyledVideo,
 } from "./styledComponent";
 
 class FourthPage extends Component {
@@ -35,22 +42,61 @@ class FourthPage extends Component {
             </Heading>
           </HeadingContaner>
           <HeadingContaner>
-            <p>
+            <Paragraph>
               Sell online, in person, and around the world with the marketing
               tools, social integrations, and sales channels you need to get
               your products in front of customers.
-            </p>
-            <a
+            </Paragraph>
+            <Anchor
               href="/sell"
               onMouseEnter={this.handleMouseEneter}
               onMouseLeave={this.handleMouseLeave}
             >
-              {arrow1 && <Arrow1 />}
-              <span>Start selling</span>
-              {arrow2 && <Arrow2 />}
-            </a>
+              <ArrowContainer>
+                <Arrow1 arrow1={arrow1} />
+                <span>Start selling</span>
+              </ArrowContainer>
+              <Arrow2 arrow2={arrow2} />
+            </Anchor>
+            <Line />
           </HeadingContaner>
         </DescriptionContainer>
+        <StyledContainer>
+          <StyledImage
+            sizes="100vw"
+            srcset="https://cdn.shopify.com/shopifycloud/brochure/assets/home/redesign2022/sell-buyer-small-f7a1e59a62226c554e7691b17a1af76c06978bd41ce78d4f980446ed1bfcaad6.webp 1x, https://cdn.shopify.com/shopifycloud/brochure/assets/home/redesign2022/sell-buyer-large-37ef6b862e53584054039ff594f868d60f1804c08624b462c4f4d679e7155c71.webp 2x"
+            loading="lazy"
+            alt="Marketing product examples"
+          />
+          <StyledImage
+            sizes="100vw"
+            srcset="https://cdn.shopify.com/shopifycloud/brochure/assets/home/redesign2022/marketing2-small-2a712ec8dc64d2b33a2ddd9346f5ebeb8d87d616831d14cb32dcef3e06302598.png 1x, https://cdn.shopify.com/shopifycloud/brochure/assets/home/redesign2022/marketing2-large-11a96ec4b8f603824a9f489b164510137d62ec4035577593d68c787159c9a5e2.png 2x"
+            loading="lazy"
+            alt="Marketing - Connect with customers online"
+          />
+          <StyledImage
+            sizes="100vw"
+            srcset="https://cdn.shopify.com/shopifycloud/brochure/assets/home/redesign2022/marketing3-small-6582aefc08f96962465ba3e4579af9e7c1fd338571c1ac96de8999feb106c05f.png 1x, https://cdn.shopify.com/shopifycloud/brochure/assets/home/redesign2022/marketing3-large-45a05b52b6219582fe516c4fdea4dfe6aa7107b7dd07b1e91e49bcf35fb85fc7.png 2x"
+            loading="lazy"
+            alt="Marketing - Connect with customers in person"
+          />
+        </StyledContainer>
+        <div>
+          <StyledVideo
+            id="wistia_simple_video_236"
+            crossorigin="anonymous"
+            poster="https://fast.wistia.com/assets/images/blank.gif"
+            aria-label="Video"
+            src="https://www.shopify.com/431ae521-b392-4eb4-bd44-850dc6ad9a06.mp4"
+            controlslist="nodownload"
+            playsinline=""
+            muted="muted"
+            loop=""
+            preload="auto"
+            type="video/m3u8"
+            x-webkit-airplay="allow"
+          ></StyledVideo>
+        </div>
       </Section>
     );
   }
--- src/components/FouthPage/styledComponent.js
+++ src/components/FouthPage/styledComponent.js
@@ -12,24 +12,58 @@ const slideIn = keyframes`
   }
 `;
 
+const slideCome = keyframes`
+  from {
+    transform: translateX(20px);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+`;
+
+const slideOut = keyframes`
+  from {
+    transform: translateX(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateX(20px);
+    opacity: 0;
+  }
+`;
+
+const slideGo = keyframes`
+  from{
+    transform: translateX(0);
+    opacity: 1;
+  }
+  to{
+    transform: translateX(-20px);
+    opacity: 0;
+  }
+`;
+
 export const Arrow1 = styled(FaArrowRightLong)`
-  margin-left: 8px;
-  animation: ${slideIn} 0.3s forwards;
-  margin-top: 3px;
-  width: 15px;
-  height: 15px;
+  animation: ${(props) => (props.arrow1 ? slideIn : slideGo)} 0.5s forwards;
+  margin-right: 0px;
+  margin-top: 5px;
+  width: 18px;
+  height: 18px;
 `;
 export const Arrow2 = styled(FaArrowRightLong)`
   margin-left: 8px;
-  animation: ${slideIn} 0.3s forwards;
+  animation: ${(props) => (props.arrow2 ? slideCome : slideOut)} 0.5s forwards;
   margin-top: 3px;
-  width: 15px;
-  height: 15px;
+  width: 18px;
+  height: 18px;
 `;
 
 export const Section = styled.section`
   width: 85%;
   height: auto;
+  margin-bottom: 4rem;
 `;
 
 export const DescriptionContainer = styled.div`
@@ -42,6 +76,12 @@ export const DescriptionContainer = styled.div`
 
 export const HeadingContaner = styled.div`
   text-align: left;
+  width: 35%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
+  height: 200px;
 `;
 
 export const Heading = styled.h1`
@@ -51,4 +91,91 @@ export const Heading = styled.h1`
 
 export const Para = styled.p`
   font-weight: 700;
+  margin: 0px;
+`;
+
+export const Anchor = styled.a`
+  position: relative;
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  color: #000;
+  font-weight: 600;
+  font-size: 18px;
+  text-decoration: none;
+  transition: transform 0.5s;
+  &&:hover {
+    transform: translateX(20px);
+  }
+`;
+
+export const Line = styled.hr`
+  width: 90%;
+  height: 1px;
+  background-color: #000;
+  padding-left: 0px;
+  align-self: flex-start;
+`;
+
+export const ArrowContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Paragraph = styled.p`
+  padding-left: 20px;
+`;
+
+export const StyledContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: var(--gutter, 16px);
+  max-width: 100vw;
+  overflow-x: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  scroll-snap-type: x mandatory;
+  @media (min-width: 768px) {
+    max-width: 100%;
+    overflow-x: visible;
+  }
+`;
+
+export const StyledImage = styled.img`
+  width: 80vw;
+  aspect-ratio: 1 / 1;
+  scroll-snap-align: center;
+  &:first-child {
+    margin-left: var(--gutter, 16px);
+  }
+  &:last-child {
+    margin-right: var(--gutter, 16px);
+  }
+  @media (min-width: 768px) {
+    width: calc(33.333% - var(--gutter, 16px));
+    &:first-child {
+      margin-left: 0;
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+`;
+
+export const StyledVideo = styled.video`
+  background: transparent;
+  display: block;
+  height: 100%;
+  max-height: none;
+  max-width: none;
+  position: static;
+  visibility: visible;
+  width: 100%;
+  object-fit: contain;
 `;
